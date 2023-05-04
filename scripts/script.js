b5f2eec76c5febc7b1fb5590f9b4d8b228d4e8c5
Mark <markkz3r0@gmail.com>
2023-05-04 15:47:22 -0700

    made image carousel

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,14 @@ let cValue = 1;
 function cIncrease() {
     cValue++;
     if (cValue > 3) {
-        cValue = 0;
+        cValue = 1;
     };
     cCycle(cValue);
 }
 
 function cDecrease() {
     cValue--;
-    if (cValue < 0) {
+    if (cValue < 1) {
         cValue = 3;
     };
     cCycle(cValue);
@@ -18,7 +18,8 @@ function cDecrease() {
 
 function cCycle(x) {
     let a = document.getElementById('cIndex').className.toString();
-    let b = a.length;
-    a = a.substring((b - 1));
-    document.getElementById('cIndex').className = a + x;
+    document.getElementById('cIndex').classList.remove(a);
+    a = a.substring(0,(a.length - 1));
+    document.getElementById('cIndex').classList.add(a + x)
+    console.log(document.getElementById('cIndex').className);
 }
